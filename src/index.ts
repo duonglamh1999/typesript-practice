96ad9ff1bbb5a9294fa6723037611ddd6c1fd6df
duonglamh1999 <duonglamh1999@gmail.com>
2022-09-28 22:38:19 -0500

    update config

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,16 @@
 //mac: touch tsconfig.json 
 
 let age:number  = 20;
+
+//declare variable
+const add = (x:number,y:number): number =>{
+    return x +y
+}
+
+//arrray with type
+let numbers: number[] = [];
+
+//tuple 
+let user: [number,string] = [1,'cute'];
+
+//
