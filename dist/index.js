c30016af37f2e3fbb8c6a18c25f4ffb51d5d555b
duonglamh1999 <duonglamh1999@gmail.com>
2022-09-28 22:19:27 -0500

    first commit

---
+++ dist/index.js
@@ -0,0 +1,5 @@
+"use strict";
+let age = 20;
+if (age < 59)
+    age += 10;
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+// create tsconfig
+//window: tsc --init
+//mac: touch tsconfig.json 
+
+let age:number  = 20;
